237bb70e83024d37b83cb71a2919a0e8a01a7001
Robert Stein <robert.stein@desy.de>
2022-04-14 16:51:40 -0700

    Update schema path

--- wintertoo/data/__init__.py
+++ wintertoo/data/__init__.py
@@ -10,7 +10,7 @@ summer_fields = pd.read_csv(summer_fields_path, sep='\s+')
 
 summer_filters = ["u", "g", "r", "i"]
 
-too_schedule_config_path = os.path.join(data_dir, "request.json")
+too_schedule_config_path = os.path.join(data_dir, "schedule_schema.json")
 
 with open(too_schedule_config_path, "rb") as f:
     too_schedule_config = json.load(f)
