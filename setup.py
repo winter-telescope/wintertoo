1198f0caac66e11f11061ab5303e588256d47ba1
Robert Stein <rdstein@caltech.edu>
2022-05-18 18:29:22 -0700

    Bump to v0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wintertoo",
-    version="0.1.0",
+    version="0.1.1",
     author="Danielle Frostig, Viraj Karambelkar, Robert Stein",
     long_description=long_description,
     long_description_content_type="text/markdown",
