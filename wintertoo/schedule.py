388d2c9cef8e7e92577eb071068189faabb2cc2f
Robert Stein <rdstein@caltech.edu>
2022-05-21 21:37:59 -0700

    Swap to convention of start/end times

--- wintertoo/schedule.py
+++ wintertoo/schedule.py
@@ -21,6 +21,8 @@ def build_schedule(
         ra_degs: list,
         dec_degs: list,
         field_ids: list,
+        start_times: list,
+        end_times: list,
         program_name: str,
         program_id: int,
         pi: str,
@@ -31,56 +33,48 @@ def build_schedule(
         dither_bool: bool = get_default_value("dither"),
         dither_distance: float = get_default_value("ditherStepSize"),
         maximum_airmass: float = get_default_value("maxAirmass"),
-        nights: list = None,
         target_priorities: list = None,
-        t_0: Time = None,
         csv_save_file: str = None,
 ):
 
-    if nights is None:
-        nights = list([1, 2, 3])
     if filters is None:
         filters = summer_filters
-    if t_0 is None:
-        t_0 = Time.now()
 
     schedule = pd.DataFrame()
 
-    for night in nights:
-        start_date = t_0 + (night-1)*u.day
-        end_date = t_0 + (night*u.day)
+    for i, ra_deg in enumerate(ra_degs):
+        dec_deg = dec_degs[i]
+        field_id = field_ids[i]
+        start_time = start_times[i]
+        end_time = end_times[i]
 
-        for i, ra_deg in enumerate(ra_degs):
-            dec_deg = dec_degs[i]
-            field_id = field_ids[i]
+        priority = calculate_overall_priority(
+            target_priority=target_priorities[i],
+            program_base_priority=program_priority
+        )
 
-            priority = calculate_overall_priority(
-                target_priority=target_priorities[i],
-                program_base_priority=program_priority
-            )
-
-            for f in filters:
-                for _ in range(nexp):
-                    new = pd.DataFrame([{
-                        "fieldRA": np.radians(ra_deg),
-                        "fieldDec": np.radians(dec_deg),
-                        "fieldID": field_id,
-                        "azimuth": -100,
-                        "altitude": -100,
-                        "filter": f,
-                        "visitExpTime": texp,
-                        "priority": priority,
-                        "programPI": pi,
-                        "progName": program_name,
-                        "progID": program_id,
-                        "validStart": start_date.mjd,
-                        "validStop": end_date.mjd,
-                        "observed": False,
-                        "maxAirmass": maximum_airmass,
-                        "dither": dither_bool,
-                        "ditherStepSize": dither_distance,
-                    }])
-                    schedule = pd.concat([schedule, new], ignore_index=True)
+        for f in filters:
+            for _ in range(nexp):
+                new = pd.DataFrame([{
+                    "fieldRA": np.radians(ra_deg),
+                    "fieldDec": np.radians(dec_deg),
+                    "fieldID": field_id,
+                    "azimuth": -100,
+                    "altitude": -100,
+                    "filter": f,
+                    "visitExpTime": texp,
+                    "priority": priority,
+                    "programPI": pi,
+                    "progName": program_name,
+                    "progID": program_id,
+                    "validStart": start_time.mjd,
+                    "validStop": end_time.mjd,
+                    "observed": False,
+                    "maxAirmass": maximum_airmass,
+                    "dither": dither_bool,
+                    "ditherStepSize": dither_distance,
+                }])
+                schedule = pd.concat([schedule, new], ignore_index=True)
 
     schedule = schedule.astype({
         "dither": bool,
@@ -102,38 +96,38 @@ def make_schedule(
         ra_degs: list,
         dec_degs: list,
         field_ids: list,
+        start_times: list,
+        end_times: list,
+        filters: list,
         target_priorities: list,
         program_name: str,
         program_id: int,
         pi: str,
         program_priority: float = 0.,
-        filters: list = None,
         t_exp: float = get_default_value("visitExpTime"),
         n_exp: int = 1,
         dither_bool: bool = get_default_value("dither"),
         dither_distance: float = get_default_value("ditherStepSize"),
         maximum_airmass: float = get_default_value("maxAirmass"),
-        nights: list = None,
-        t_0: Time = None,
         csv_save_file: str = None,
 ):
     schedule = build_schedule(
         ra_degs=ra_degs,
         dec_degs=dec_degs,
         field_ids=field_ids,
+        start_times=start_times,
+        end_times=end_times,
+        filters=filters,
         target_priorities=target_priorities,
         program_name=program_name,
         program_id=program_id,
         pi=pi,
         program_priority=program_priority,
-        filters=filters,
         texp=t_exp,
         nexp=n_exp,
         dither_bool=dither_bool,
         dither_distance=dither_distance,
         maximum_airmass=maximum_airmass,
-        nights=nights,
-        t_0=t_0,
         csv_save_file=csv_save_file,
     )
     return schedule
@@ -146,23 +140,37 @@ def schedule_ra_dec(
         program_name: str,
         program_id: int,
         target_priority: float = 1.,
-        filters: list = None,
+        filters=None,
         t_exp: float = get_default_value("visitExpTime"),
         n_exp: int = 1,
         dither_bool: bool = get_default_value("dither"),
         dither_distance: float = get_default_value("ditherStepSize"),
-        maximum_airmass: float = get_default_value("maxAirmass"),
-        nights=[1],
-        t_0=None,
+        start_time: Time = None,
+        end_time: Time = None,
         summer: bool = True,
         use_field: bool = True,
         csv_save_file: str = None,
 ):
 
+    if start_time is None:
+        logger.info(f"No start time specified. Using 'now' as start time.")
+        start_time = Time.now()
+
+    if end_time is None:
+        logger.info(f"No end time specified. Using '1 day from now' as end time.")
+        end_time = Time.now() + 1*u.day
+
     if summer:
         get_best_field = get_best_summer_field
+        default_filters = summer_filters
     else:
         raise NotImplementedError
+
+    if filters is None:
+        logger.info(f"No filters specified. Using all as default: {default_filters}.")
+        filters = default_filters
+    if not isinstance(filters, list):
+        filters = [filters]
 
     # Take RA/Dec and select nearest-centered field
 
@@ -175,17 +183,17 @@ def schedule_ra_dec(
         field_id = get_default_value("fieldID")
 
     schedule = make_schedule(
-        ra_degs=[ra_deg],
-        dec_degs=[dec_deg],
-        field_ids=[field_id],
-        target_priorities=[target_priority],
+        ra_degs=[ra_deg for _ in filters],
+        dec_degs=[dec_deg for _ in filters],
+        field_ids=[field_id for _ in filters],
+        start_times=[start_time for _ in filters],
+        end_times=[end_time for _ in filters],
         filters=filters,
+        target_priorities=[target_priority for _ in filters],
         t_exp=t_exp,
         n_exp=n_exp,
         dither_bool=dither_bool,
         dither_distance=dither_distance,
-        nights=nights,
-        t_0=t_0,
         pi=pi,
         program_name=program_name,
         program_id=program_id,
