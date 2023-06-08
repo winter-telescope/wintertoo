2a1c68655310bc5f1617ce69b48c7807f1d3cdfd
Robert Stein <rdstein@caltech.edu>
2023-06-07 17:55:55 -0700

    More testing

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -10,7 +10,11 @@ import pandas as pd
 
 from wintertoo.models import Program, SummerRaDecToO
 from wintertoo.schedule import schedule_ra_dec
-from wintertoo.validate import validate_schedule_df
+from wintertoo.validate import (
+    validate_obshist,
+    validate_schedule_df,
+    validate_target_visibility,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -44,8 +48,8 @@ class TestSchedule(unittest.TestCase):
             too=SummerRaDecToO(
                 ra_deg=173.7056754,
                 dec_deg=11.253441,
-                start_time_mjd=69721.1894969287,
-                end_time_mjd=69722.1894969452,
+                start_time_mjd=62721.1894969287,
+                end_time_mjd=62722.1894969452,
             ),
             program=Program(
                 pi_name="Stein",
@@ -56,6 +60,9 @@ class TestSchedule(unittest.TestCase):
                 enddate=date(2023, 12, 31),
             ),
         )
+
+        validate_target_visibility(schedule)
+
         comp = pd.read_json(schedule.to_json())  # pylint: disable=no-member
         self.assertEqual(test_df.to_json(), comp.to_json())  # pylint: disable=no-member
 
@@ -74,6 +81,5 @@ class TestSchedule(unittest.TestCase):
             ),
         )
 
-        print(schedule)
-
         validate_schedule_df(schedule)
+        validate_obshist(schedule)
